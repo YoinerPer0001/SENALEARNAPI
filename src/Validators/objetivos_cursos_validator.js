1e41fa29de42805d1cd2dd4ed205c4af957ebca3
Aprendiz SENA <yoinerpertuz@gmail.com>
2024-05-30 10:24:29 -0500

    editar objetivos 200 ok

--- src/Validators/objetivos_cursos_validator.js
+++ src/Validators/objetivos_cursos_validator.js
@@ -28,12 +28,12 @@ export const createValidation = [
 
 export const UpdateValidation = [
     check('Desc_Objetivo')
-        .exists().withMessage('Desc_Objetivo is required')
+        .optional()
         .not().isEmpty().withMessage('Desc_Objetivo cannot be empty')
         .isString().withMessage('Desc_Objetivo must be a string')
         .isLength({ max: 255 }).withMessage('max length is 255'),
     check('Id_Cur')
-        .exists().withMessage('Id_Cur is required')
+        .optional()
         .not().isEmpty().withMessage('Id_Cur cannot be empty')
         .isString().withMessage('Id_Cur must be a string')
         .isLength({ max: 100 }).withMessage('max length is 100'),
